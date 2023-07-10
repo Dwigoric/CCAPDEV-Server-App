a01e202f55b793611da8b8a1e029e044443598db
Joshua Permito <dwigoric@insanedevs.com>
2023-07-10 19:25:39 +0800

    chore: remove unnecessary files from generator + set up module type package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+    // Send a JSON response with 200 OK
+    res.status(200).json({ message: 'Hello, world!' })
+})
 
-module.exports = router;
+export default router
