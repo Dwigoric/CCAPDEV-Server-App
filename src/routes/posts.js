4185fe3f39c773e5f0c7994d8e0b8b4050f95f6e
Joshua Permito <dwigoric@insanedevs.com>
2023-07-16 06:56:01 +0800

    feat: PUT /posts

---
+++ src/routes/posts.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { mongo } from '../db/conn.js'
+import { v5 as uuidV5 } from 'uuid'
+
+const router = express.Router()
+
+router.put('/posts', async (req, res, next) => {
+    if (!(await mongo.hasTable('posts'))) await mongo.createTable('posts')
+
+    const { userId, title, body, image } = req.body
+
+    let post
+    try {
+        post = await mongo.create('posts', uuidV5(Date.now(), uuidV5.URL), {
+            userId,
+            title,
+            body,
+            image,
+            date: new Date(),
+            edited: false
+        })
+    } catch (err) {
+        return res.status(500).json({ error: true, message: err.message })
+    }
+
+    delete post._id
+    return res.status(201).json({ post, message: 'Post created' })
+})
+
+export default router
