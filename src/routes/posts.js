f37bc9c9014d22ec080b3b3272b522a639cb586c
Joshua Permito <dwigoric@insanedevs.com>
2023-07-17 08:54:33 +0800

    feat(/posts): GET /:id for specific post retrieval

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -59,4 +59,13 @@ router.get('/', async (req, res, next) => {
     })
 })
 
+router.get('/:id', async (req, res, next) => {
+    const { id } = req.params
+
+    const post = await mongo.get('posts', id)
+    if (!post) return res.status(404).json({ error: true, message: 'Post not found' })
+
+    return res.status(200).json({ post, message: 'Post found' })
+})
+
 export default router
