5024b311665fa8cff4f532c3a5ec4f6f7409a753
Joshua Permito <dwigoric@insanedevs.com>
2023-07-23 23:47:37 +0800

    refactor(votes): restructure code

--- src/routes/votes.js
+++ src/routes/votes.js
@@ -3,86 +3,73 @@ import { mongo } from '../db/conn.js'
 
 const router = express.Router()
 
-router.patch('/:id', async (req, res)  => {
-     
-     const { id } = req.params
-     const { userId, vote } = req.body
+router.post('/:id', async (req, res) => {
+    const { id } = req.params
+    const { userId, vote } = req.body
 
-     const isLoggedIn = await mongo.findOne('users', { id: userId })
-     if (!isLoggedIn) {
-          return res.status(500).json({ error: true, message: 'Not logged in' })
-     }
-     //Find the post
-     const post = await mongo.get('posts', { id: id })
-     if (!post) return res.status(404).json({ error: true, message: 'Post not found' })
-     
-     //votesIndex
-     const votesIndex = {
-          userId: userId,
-          reactions: vote
-     }
-     //Create  a new  table
-     if (!(await mongo.hasTable('votes')))  { 
-          await mongo.createTable('votes')
-     }
-   
-     //await mongo.findOne('votes', { 'id': id})
-     const isVoted  = await mongo.findOne('votes', { id: id, userId: userId })
+    // Find the user
+    const user = await mongo.get('users', userId)
+    if (!user) {
+        return res.status(404).json({ error: true, message: 'User not found' })
+    }
 
-     let newReactions = post.reactions
+    // Find the post
+    const post = await mongo.get('posts', { id: id })
+    if (!post) return res.status(404).json({ error: true, message: 'Post not found' })
 
-     if (isVoted) {
-          if (isVoted.reactions == 1) {
-               newReactions -= 1
-          }
-          else if (isVoted.reactions == -1) {
-               newReactions += 1
-          }
+    //votesIndex
+    const votesIndex = {
+        userId: userId,
+        reactions: vote
+    }
+    // Create a new  table
+    if (!(await mongo.hasTable('votes'))) {
+        await mongo.createTable('votes')
+    }
 
-          newReactions += vote
+    const isVoted = await mongo.findOne('votes', { id: id, userId: userId })
 
-          try {
-               mongo.update('votes', id, votesIndex)
-          } catch (err) {
-               return res.status(500).json({ error: true, message: err.message })
-          }
-     }
-     else {
-          newReactions += vote
+    let newReactions = post.reactions
 
-          try {
-               await mongo.create('votes', id, votesIndex)
-          } catch (err) {
-               return res.status(500).json({ error: true, message: err.message })
-          }
-     }
+    if (isVoted) {
+        if (isVoted.reactions === 1) {
+            newReactions -= 1
+        } else if (isVoted.reactions === -1) {
+            newReactions += 1
+        }
+    }
 
-     
+    newReactions += vote
 
-     const updatedVote = {
-          ...post,
-          reactions: newReactions,
-     }
-     try {
-          await mongo.update('posts', id, updatedVote)
-     } catch (err) {
-          return res.status(500).json({ error: true, message: err.message })
-     }
+    try {
+        await mongo.create('votes', id, votesIndex)
+    } catch (err) {
+        return res.status(500).json({ error: true, message: err.message })
+    }
 
-     return res.status(200).json({ post: updatedVote, message: 'vote changed' })
-    
+    const updatedVote = {
+        ...post,
+        reactions: newReactions
+    }
+    try {
+        await mongo.update('posts', id, updatedVote)
+    } catch (err) {
+        return res.status(500).json({ error: true, message: err.message })
+    }
+
+    return res.status(200).json({ post: updatedVote, message: 'Vote changed' })
 })
-router.put('/:id', async (req, res) =>{
-     const { id } = req.params
-     const { userId } = req.body
 
-     const isVoted  = await mongo.findOne('votes', { id: id, userId: userId })
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const { userId } = req.query
 
-     if (isVoted) {
-          return res.status(200).json({message: "Voted already", reactions: isVoted.reactions})
-     }
-     else {
-          return res.status(500).json({ error: true, message: "Hasn't been voted", reactions: 0 })
-     }
+    const isVoted = await mongo.findOne('votes', { id: id, userId: userId })
+
+    if (isVoted) {
+        return res.status(100).json({ reactions: isVoted.reactions, message: 'Votes retrieved' })
+    } else {
+        return res.status(100).json({ reactions: 0, message: 'No votes yet' })
+    }
 })
 export default router
